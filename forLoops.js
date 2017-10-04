0d1eb92792de098a164b30f01848ded970a0e2af
Candice Bianca Brown <candicebrown97@gmail.com>
2017-10-04 03:33:59 +0000

    exercise 1

---
+++ forLoops.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p id="demo"></p>
+
+<script>
+var i;
+for (i = num1; i < num2; i++) {
+    document.getElementById("demo").innerHTML += i + "<br>";
+}
+</script>
+
+</body>
+</html>
+
+//Result
+0 thru 9 // on new line
