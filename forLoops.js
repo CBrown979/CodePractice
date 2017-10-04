26afd3ae831b8450ac6740ab895ed4257604c91f
Candice Bianca Brown <candicebrown97@gmail.com>
2017-10-04 03:44:21 +0000

    added loop to countdown from 10 to 1

--- forLoops.js
+++ forLoops.js
@@ -16,3 +16,21 @@ for (i = num1; i < num2; i++) {
 
 //Result
 0 thru 9 // on new line
+
+//Make the loop start counting downwards from 10 and stop at 1.
+//The output should be 10 9 8 7 6 5 4 3 2 1 with line breaks.
+<!DOCTYPE html>
+<html>
+<body>
+
+<p id="demo"></p>
+
+<script>
+var i;
+for (i = 10; i > 0; i--) {
+    document.getElementById("demo").innerHTML += i + "<br>";
+}
+</script>
+
+</body>
+</html>
