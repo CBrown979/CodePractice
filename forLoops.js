f246660150c2d241f08630bb9f993e540226e9de
Candice Bianca Brown <candicebrown97@gmail.com>
2017-10-04 03:48:08 +0000

    loop thru length of array

--- forLoops.js
+++ forLoops.js
@@ -34,3 +34,34 @@ for (i = 10; i > 0; i--) {
 
 </body>
 </html>
+
+//Inside the for loop: 
+// Set the counter variable (i) to 0. 
+// Run the loop as long as i is less than the length of the food array. 
+// Increment i by 1.
+<html>
+<body>
+
+<p id="demo"></p>
+
+<script>
+var text = "";
+var food = ["Pizza", "Beans", "Tacos", "Fish", "Chicken"];
+var i;
+
+for (i = 0; i < food.length; i++) {
+    text += "I love " + food[i] + "<br>";
+}
+
+document.getElementById("demo").innerHTML = text;
+</script>
+
+</body>
+</html>
+
+Result:
+I love Pizza
+I love Beans
+I love Tacos
+I love Fish
+I love Chicken
